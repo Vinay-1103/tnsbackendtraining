a01f87842ce66c5eada9d201c86b1a230de7b10b
Vinay-1103 <vinaykumar22ra1a6694@gmail.com>
2025-08-04 21:23:08 +0530

    add files

---
+++ FirstOne.java
@@ -0,0 +1,6 @@
+public class FirstOne {
+
+    public void displayDetails(){
+        System.out.println("Welcome To TNS");
+    }
+}
---
+++ FirstOneMain.java
@@ -0,0 +1,7 @@
+public class FirstOneMain {
+
+    public static void main(String[] args) {
+        FirstOne fo=new FirstOne();
+        fo.displayDetails();
+    }
+}
