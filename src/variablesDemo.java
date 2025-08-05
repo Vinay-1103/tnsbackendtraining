76df7ce62ea34815af4b94868deb63900b6f5ed9
Vinay-1103 <vinaykumar22ra1a6694@gmail.com>
2025-08-05 20:34:10 +0530

    add files

---
+++ src/variablesDemo.java
@@ -0,0 +1,13 @@
+public class variablesDemo {
+
+
+    static String college="KPRIT";
+
+    int rollno=50;
+
+    void display(){
+        String name="Vinay";
+        System.out.println(name);
+    }
+
+}
---
+++ src/variablesDemoMain.java
@@ -0,0 +1,10 @@
+public class variablesDemoMain {
+
+    public static void main(String[] args) {
+        variablesDemo v1=new variablesDemo();
+        v1.display();
+//        int roll=v1.rollno;
+        System.out.println(v1.rollno);
+        System.out.println(variablesDemo.college);
+    }
+}
