c415fec8c9c95fef51dd252ba04c77692cc0d9b0
Vinay-1103 <vinaykumar22ra1a6694@gmail.com>
2025-08-08 20:34:47 +0530

    add files

---
+++ src/ForLoopDemo.java
@@ -0,0 +1,16 @@
+public class ForLoopDemo {
+        public static void main(String[] args) {
+//        for (int i=1;i<=100;i++){
+//            System.out.println("Welcome , Good mrng");
+//        }
+//        int i=1;
+//        int j=20;
+//    for ( i=2,j=10;i<=j;i++,j--);{
+//        System.out.println(i);
+//    }
+            for (int i=1,j=10;i<=10 &&j>=1;i++,j--){
+                System.out.println(i);
+            }
+        }
+    }
+
