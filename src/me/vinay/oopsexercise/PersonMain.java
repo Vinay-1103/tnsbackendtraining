f4f2e06dd79a44cb3f070ff6da5dc35a0fe01a83
Vinay-1103 <vinaykumar22ra1a6694@gmail.com>
2025-08-12 21:15:36 +0530

    add files

--- src/me/vinay/oopsexercise/PersonMain.java
+++ src/me/vinay/oopsexercise/PersonMain.java
@@ -3,6 +3,7 @@ package me.vinay.oopsexercise;
 public class PersonMain {
         public static void main(String[] args) {
             Person p1=new Person("Vinay",21,"male");
+            Person p2=new Person("kumar",21,"male");
             p1.display();
         }
     }
