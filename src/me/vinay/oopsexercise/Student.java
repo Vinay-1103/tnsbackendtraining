2bb4df4b7f916ea76a0fe0b1a877c8650baa1275
Vinay-1103 <vinaykumar22ra1a6694@gmail.com>
2025-08-12 21:22:14 +0530

    add files

---
+++ src/me/vinay/oopsexercise/Student.java
@@ -0,0 +1,26 @@
+package me.vinay.oopsexercise;
+
+public class Student {
+    private int sid;
+    private String sname;
+
+    public int getSid() {
+        return sid;
+    }
+    void display(){
+        System.out.println(this.sid);
+        System.out.println(this.sname);
+    }
+
+    public void setSid(int sid) {
+        this.sid = sid;
+    }
+
+    public String getSname() {
+        return sname;
+    }
+
+    public void setSname(String sname) {
+        this.sname = sname;
+    }
+}
