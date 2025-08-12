380f1bf3a5104c44b9f3ecafe3e0a4babe114ad9
Vinay-1103 <vinaykumar22ra1a6694@gmail.com>
2025-08-12 21:22:41 +0530

    add files

---
+++ src/me/vinay/oopsexercise/StudentMain.java
@@ -0,0 +1,10 @@
+package me.vinay.oopsexercise;
+
+public class StudentMain {
+    public static void main(String[] args) {
+        Student ob=new Student();
+        ob.setSid(94);
+        ob.display();
+
+    }
+}
