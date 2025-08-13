64780737fdf31f29678167841df761a0c8cd68c9
Vinay-1103 <vinaykumar22ra1a6694@gmail.com>
2025-08-13 23:13:27 +0530

    add files

---
+++ src/me/vinay/tns/assignments/TicketBookingMain.java
@@ -0,0 +1,53 @@
+package me.vinay.tns.assignments;
+
+import java.util.Scanner;
+
+public class TicketBookingMain {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String[] aa=sc.nextLine().split(",");
+        String event=aa[0];//sc.nextLine();
+        String customer=aa[1];//sc.nextLine();
+        int seats=Integer.parseInt(aa[2]);//sc.nextInt();
+
+        TicketBooking tb=new TicketBooking(event,customer,seats);
+        System.out.println("Payment choice:");
+        System.out.println("1.Cash payment");
+        System.out.println("2.Wallet payment");
+        System.out.println("3.Credit card payment");
+
+        int choice= sc.nextInt();
+        switch (choice){
+            case 1 -> {
+                System.out.println("Enter amount");
+                double amount= sc.nextDouble();
+                tb.display();
+                tb.makePayment(amount);
+            }
+            case 2->{
+                System.out.println("Enter wallet No.");
+                String walletNo=sc.nextLine();
+                sc.nextLine();
+                System.out.println("Enter amount");
+                double amount=sc.nextDouble();
+                tb.display();
+                tb.makePayment(walletNo,amount);
+            }
+            case 3 ->{
+                System.out.println("Enter credit Card Number");
+                String credicard=sc.nextLine();
+                sc.nextLine();
+                System.out.println("Enter CVV ");
+                String cvv=sc.nextLine();
+                System.out.println("Enter cardholder name");
+                String name=sc.nextLine();
+               // sc.nextLine();
+                System.out.println("Enter amount");
+                double amount = sc.nextDouble();
+                tb.display();
+                tb.makePayment(credicard,cvv,name,amount);
+            }
+            default -> System.out.println("Invalid choice");
+        }
+    }
+}
