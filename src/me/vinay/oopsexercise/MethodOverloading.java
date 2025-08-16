269cc1fefcbea49dc35f2cdf9aceb23e7f5a077a
Vinay-1103 <vinaykumar22ra1a6694@gmail.com>
2025-08-16 22:42:18 +0530

    add files

---
+++ src/me/vinay/oopsexercise/MethodOverloading.java
@@ -0,0 +1,25 @@
+package me.vinay.oopsexercise;
+
+public class MethodOverloading {
+        static  void print(int a){
+            System.out.println("Printing integer value: "+a);
+        }
+        static  void print(double a){
+            System.out.println("Printing double value: "+a);
+        }
+        static  void print(String a){
+            System.out.println("Printing String value: "+a);
+        }
+        double add(int a,double b){
+            return a+b;
+        }
+
+        public static void main(String[] args) {
+            MethodOverloading obj=new MethodOverloading();
+            System.out.println(obj.add(1,20)) ;
+//        print(5);
+//        print("Vinay");
+//        print(8.5);
+        }
+    }
+
