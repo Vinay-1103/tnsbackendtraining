0cf05065a4fa53f0011bce1ec536174d9d7f03bb
Vinay-1103 <vinaykumar22ra1a6694@gmail.com>
2025-08-16 22:45:59 +0530

    add files

---
+++ src/forEachLoop.java
@@ -0,0 +1,34 @@
+public class forEachLoop {
+        public static void main(String[] args) {
+            int[] number=new int[]{4,8,9,7,6,5};
+            for (int n : number){
+                System.out.println(n);
+            }
+            //printing sum of all elements of an integer array
+            int[] numbers=new int[]{4,8,9,7,6,5};
+            int sum=0;
+            for (int n : numbers){
+                sum +=n;
+            }
+            System.out.println(sum);
+
+            //count of characters
+            char[] ch={'a','b','c','a','b','a'};
+            char tar='b';
+            int count=0;
+            for (char i:ch){
+                if(i == tar){
+                    count++;
+                }
+            }
+            System.out.println(count);
+
+//        print all even no.s from array
+            for (int num:numbers){
+                if (num%2==0){
+                    System.out.print(num+" ");
+                }
+            }
+        }
+    }
+
