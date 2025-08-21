4a191fa6b271715dadb36a04fd6ff4e65e584e88
Vinay-1103 <vinaykumar22ra1a6694@gmail.com>
2025-08-21 22:05:54 +0530

    add files

---
+++ src/tns/tns/assignments/ATM.java
@@ -0,0 +1,32 @@
+package tns.tns.assignments;
+
+public  class ATM {
+    private double balance=10000.94;
+    private final int validPassword=1103;
+
+
+     int validatePassword(int password) throws InvalidPinException{
+        if (password!=validPassword){
+            throw new InvalidPinException("Password is not valid.Enter correct password");
+        }
+        System.out.println("Authentication sucessfully Done");
+        return 1;
+    }
+     void withdraw(double amount) throws InsufficientBalanceException{
+        if (amount>balance){
+            throw new InsufficientBalanceException("Insufficent balance,Enter valid amount");
+        }
+         balance-=amount;
+         System.out.printf("Withdrawn amount :%.3f |Balance in Account :%.3f ",amount,balance);
+         System.out.println();
+    }
+
+    void balanceInAccount(){
+        System.out.println(balance);
+    }
+    void addAmount(double amount) {
+         balance+=amount;
+        System.out.printf("Amount added :%.3f|Balance in Account :%.3f ",amount,balance);
+        System.out.println();
+    }
+}
