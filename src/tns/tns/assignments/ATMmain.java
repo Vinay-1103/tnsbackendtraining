11b621de7ac88561b8a0e180fb8497ffda79fa34
Vinay-1103 <vinaykumar22ra1a6694@gmail.com>
2025-08-21 22:06:15 +0530

    add files

---
+++ src/tns/tns/assignments/ATMmain.java
@@ -0,0 +1,47 @@
+package tns.tns.assignments;
+
+import java.util.Scanner;
+
+public class ATMmain {
+    public static void main(String[] args) {
+            ATM atm=new ATM();
+            Scanner sc=new Scanner(System.in);
+        try {
+            String option;
+            System.out.print("Enter Password :");
+            int pass=sc.nextInt();
+            int result=atm.validatePassword(pass);
+            do{
+            if (result==1){
+                System.out.println("*******Welcome*******");
+                System.out.println("1.CheckBalance");
+                System.out.println("2.Withdraw");
+                System.out.println("3.Add amount");
+                int choice= sc.nextInt();
+                switch (choice){
+                    case 1 ->{
+                        atm.balanceInAccount();
+                    }
+                    case 2->{
+                        System.out.println("Enter amount :");
+                        double amount=sc.nextDouble();
+                        atm.withdraw(amount);
+                    }
+                    case 3->{
+                        System.out.println("Enter amount :");
+                        double amount=sc.nextDouble();
+                        atm.addAmount(amount);
+                    }
+                    default -> System.out.println("Invalid choice");
+                }
+            }
+                System.out.println("Enter 1 for goto options ,press any key for Exit ");
+                option=sc.next();
+            }while (option.equals("1"));
+        }catch (InvalidPinException e){
+            System.out.println(e.getMessage());
+        } catch (InsufficientBalanceException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
