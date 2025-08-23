6fdaabcc256d151852f04c5dbe1cf585673be90d
Vinay-1103 <vinaykumar22ra1a6694@gmail.com>
2025-08-23 22:10:49 +0530

    add files

---
+++ src/me/vinay/tns/oopsexercise/abstraction/Bike.java
@@ -0,0 +1,8 @@
+package me.vinay.tns.oopsexercise.abstraction;
+
+public class Bike extends Vehicle{
+    @Override
+    void fuel() {
+        System.out.print("petrol ");
+    }
+}
---
+++ src/me/vinay/tns/oopsexercise/abstraction/EvVehicle.java
@@ -0,0 +1,8 @@
+package me.vinay.tns.oopsexercise.abstraction;
+
+public class EvVehicle extends  Vehicle{
+    @Override
+    void fuel() {
+        System.out.print("Electric city ");
+    }
+}
