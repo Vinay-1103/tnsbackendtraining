ad89aadd1d10883ca681c676f8f7f79fd86a4c79
Vinay kumar Korakoppula <vinaykumar22ra1a6694@gmail.com>
2025-08-31 21:48:35 +0530

    Update InsufficientBalanceException.java

--- src/me/vinay/tns/tns/assignments/InsufficientBalanceException.java
+++ src/me/vinay/tns/tns/assignments/InsufficientBalanceException.java
@@ -3,5 +3,6 @@ package me.vinay.tns.tns.assignments;
 public class InsufficientBalanceException extends Exception{
     public InsufficientBalanceException(String msg){
         super(msg);
+        
     }
 }
