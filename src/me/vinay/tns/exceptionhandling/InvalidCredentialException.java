f974403e7868a42feac62831421860a31f238987
Vinay-1103 <vinaykumar22ra1a6694@gmail.com>
2025-09-03 22:27:17 +0530

    add files

---
+++ src/me/vinay/tns/exceptionhandling/InvalidCredentialException.java
@@ -0,0 +1,12 @@
+package me.vinay.tns.exceptionhandling;
+
+public class InvalidCredentialException extends RuntimeException {
+  int errorcode=123;
+  public InvalidCredentialException(String msg){
+    super(msg);
+  }
+  public String toString(){
+    return "ErrorCode: "+errorcode + getMessage();
+  }
+
+}
