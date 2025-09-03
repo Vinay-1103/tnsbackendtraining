80edd5c0f861d9a584f00aa447f1f4be38d95d50
Vinay-1103 <vinaykumar22ra1a6694@gmail.com>
2025-09-03 22:26:20 +0530

    add files

---
+++ src/me/vinay/tns/exceptionhandling/CustomException.java
@@ -0,0 +1,22 @@
+package me.vinay.tns.exceptionhandling;
+
+public class CustomException {
+        static void authrnticate(String Username,String password) throws InvalidCredentialException {
+            String vaildUsename = "Vinay";
+            String validpassword = "Vinay@786";
+            if (!Username.equals(vaildUsename) || !password.equals(validpassword)){
+                throw new InvalidCredentialException("Invalid username or password");
+            }
+            System.out.println("Sucessful");
+        }
+
+        public static void main(String[] args) {
+            try {
+                authrnticate("Vinay","vinay@786");
+            }catch (InvalidCredentialException e){
+                System.out.println(e);
+            }
+        }
+
+    }
+
