d9d24722a6ce4a3b4b457fcd35b590abffb4ae7c
Vinay-1103 <vinaykumar22ra1a6694@gmail.com>
2025-09-15 17:48:59 +0530

    add files

---
+++ src/me/vinay/tns/arrays/genericdatatype/StudentMain.java
@@ -0,0 +1,16 @@
+package me.vinay.tns.arrays.genericdatatype;
+
+public class StudentMain {
+
+        public static void main(String[] args) {
+            Student[] s1 = new Student[3]; //here accessed using Classname
+            s1[0] = new Student(97, "yuvi", "A");
+            s1[1] = new Student(98, "ram", "A");
+            s1[2] = new Student(99, "ss", "B");
+
+            for(Student val: s1){
+                System.out.print(val +" "+"\n");
+            }
+        }
+    }
+
