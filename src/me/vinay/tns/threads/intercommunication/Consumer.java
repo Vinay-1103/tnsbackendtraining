db870eb660d1fd83a9d8c6dfc096ff345e72fc98
Vinay-1103 <vinaykumar22ra1a6694@gmail.com>
2025-09-19 21:29:57 +0530

    add files

---
+++ src/me/vinay/tns/threads/intercommunication/Consumer.java
@@ -0,0 +1,24 @@
+package me.vinay.tns.threads.intercommunication;
+
+public class Consumer extends  Thread{
+    public void start() {
+    }
+
+        private SharedResource resource;
+
+        Consumer(SharedResource resource) {
+            this.resource = resource;
+        }
+
+        public void run() {
+            for (int i = 1; i <= 5; i++) { // Consuming 5 items
+                resource.consumer();
+//            try {
+//                Thread.sleep(800); // Simulate time to consume
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+            }
+        }
+    }
+
