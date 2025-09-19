713bf58ae771b7e3cf6ae3d26a1b1632e9a28962
Vinay-1103 <vinaykumar22ra1a6694@gmail.com>
2025-09-19 21:30:29 +0530

    add files

---
+++ src/me/vinay/tns/threads/intercommunication/Producer.java
@@ -0,0 +1,22 @@
+package me.vinay.tns.threads.intercommunication;
+
+public class Producer extends Thread {
+
+        private SharedResource resource;
+
+        Producer(SharedResource resource) {
+            this.resource = resource;
+        }
+
+        public void run() {
+            for (int i = 1; i <= 5; i++) { // Producing 5 items
+                resource.producer(i);
+//            try {
+//                Thread.sleep(500); // Simulate time to produce
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+            }
+        }
+    }
+
