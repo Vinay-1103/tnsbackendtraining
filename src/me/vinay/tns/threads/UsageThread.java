7bb1c3fb4cef5683cd145cbcab2f178f8e2f01dc
Vinay-1103 <vinaykumar22ra1a6694@gmail.com>
2025-10-06 20:17:09 +0530

    add files

---
+++ src/me/vinay/tns/threads/UsageThread.java
@@ -0,0 +1,14 @@
+package me.vinay.tns.threads;
+
+public class UsageThread extends Thread{
+
+
+        public synchronized void mul(int n){
+            for(int i=0; i<11; i++){
+                int result = n*i;
+                System.out.println(result);
+            }
+        }
+
+    }
+
