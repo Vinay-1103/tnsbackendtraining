e623cf4bcd81553bb09347442352f4b81ae7e5eb
Vinay-1103 <vinaykumar22ra1a6694@gmail.com>
2025-10-07 22:18:23 +0530

    add files

---
+++ src/me/vinay/tns/collections/comparable/Student.java
@@ -0,0 +1,30 @@
+package me.vinay.tns.collections.comparable;
+
+public class Student  implements Comparable<Student> {
+        //can compare myself with another object
+        //since compareTo(classname object) is abstract method it must be implemented
+        int sid;
+        String name;
+        int marks;
+
+        public Student(int sid, String name, int marks){
+            super();
+            this.sid = sid;
+            this.name = name;
+            this.marks = marks;
+        }
+
+        // compareTo method (natural ordering by marks)
+        @Override
+        public int compareTo(Student s1) {
+            return this.marks - s1.marks;  // ascending order
+            // return s.marks - this.marks; // descending order
+        }
+
+        @Override
+        public String toString() {
+            return sid + " " + name + " " + marks;
+        }
+
+    }
+
